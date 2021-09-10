cbc24c009e225ed58d6f3b3002c9c63f0803cf0f
saboop99 <rafa4021@hotmail.com>
2021-09-10 17:02:09 -0300

    Criação da classe Calculadora

--- CalculoCirculo/Calculadora.cs
+++ CalculoCirculo/Calculadora.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CalculoCirculo
+//Rafael Do Nascimento Rodrigues RA 210211
 {
     class Calculadora
     {
+        //declarando variavel Pi
+        public double Pi = 3.14;
+        //calculo da Circuferencia
+        public double Circuferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        //Calculo do volume
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
     }
 }
--- CalculoCirculo/Program.cs
+++ CalculoCirculo/Program.cs
@@ -7,33 +7,25 @@ namespace CalculoCirculo
     class Program
         //Rafael Do Nascimento Rodrigues RA 210211
     {
-        //declarando variavel Pi
-        static double Pi = 3.14;
+        
         static void Main(string[] args)
         {
+            //Declaração e referencia da classe Calculadora
+            Calculadora calc = new Calculadora();
             //Mostrar na tela para o usuario digitar o valor do raio
             Console.WriteLine("Entre com o valor do raio:");
             //ler o valor escrito e converter em double, declarando variável raio
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             // declarando variavel circ (circuferencia)
-            double circ = Circuferencia(raio);
+            double circ = calc.Circuferencia(raio);
             //declarando variavel volume (Volume)
-            double volume = Volume(raio);
+            double volume = calc.Volume(raio);
 
             //Conversão dos resultados para string, para ser possível mostra-los na tela
             Console.WriteLine("Circuferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
-        //calculo da Circuferencia
-        static double Circuferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-       //Calculo do volume
-        static double Volume (double r)
-        {
-            return 4.0/3.0 * Pi * r*r*r;
-        }
+        
     }
 }
